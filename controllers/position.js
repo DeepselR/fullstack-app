48dc88c0d2f8b0b62797b3a68a84a8c5f678d983
Roman <deepselr@gmail.com>
2018-11-13 17:12:09 +0300

    Добавлени всех роутов

---
+++ controllers/position.js
@@ -0,0 +1,12 @@
+module.exports.getByCategoryId = function(req, res) {
+
+};
+module.exports.create = function(req, res) {
+
+};
+module.exports.delete = function(req, res) {
+
+};
+module.exports.update = function(req, res) {
+
+};
