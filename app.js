df0c3d662c5b3d845319365bb640e2943f7d1b2f
Roman <deepselr@gmail.com>
2018-11-21 16:28:50 +0300

    Connect to MongoDb

--- app.js
+++ app.js
@@ -2,20 +2,33 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const authRoutes = require("./routes/auth");
 const analyticsRoutes = require("./routes/analytics");
 const categoryRoutes = require("./routes/category");
 const orderRoutes = require("./routes/order");
 const positionRoutes = require("./routes/position");
+const keys = require("./config/keys");
 const app = express();
 
+mongoose
+  .connect(
+    keys.mongoUri,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch(reason => {
+    console.log(reason);
+  });
 
 app.use(morgan("dev"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors())
+app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/analytics", analyticsRoutes);
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoUri: "mongodb://deepsel:dadking12@ds047945.mlab.com:47945/fullstack"
+};
