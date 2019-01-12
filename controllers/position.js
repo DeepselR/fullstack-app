3aeb9d1d5db0c9fde1eced730af2e780db1adbf8
Roman Martsinovich <martsinovichroman@gmail.com>
2019-01-12 11:27:08 +0300

    position by category id

--- controllers/position.js
+++ controllers/position.js
@@ -1,12 +1,38 @@
-module.exports.getByCategoryId = function(req, res) {
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler');
+
+module.exports.getByCategoryId = async function (req, res) {
+    try {
+        const positions = await Position.find({
+            category: req.params.categoryId,
+            user: req.user
+        });
+        res.status(200).json(positions);
+    } catch (e) {
+        errorHandler(e, res);
+    }
+};
+module.exports.create = function (req, res) {
+    try {
+
+    } catch (e) {
+        errorHandler(e, res);
+    }
 
 };
-module.exports.create = function(req, res) {
+module.exports.delete = function (req, res) {
+    try {
+
+    } catch (e) {
+        errorHandler(e, res);
+    }
 
 };
-module.exports.delete = function(req, res) {
+module.exports.update = function (req, res) {
+    try {
 
-};
-module.exports.update = function(req, res) {
+    } catch (e) {
+        errorHandler(e, res);
+    }
 
 };
