a7b75bb185f2f43000f4af50c0c91b8f9742f805
Roman Martsinovich <martsinovichroman@gmail.com>
2019-01-12 12:46:56 +0300

    Category API and image uploader

--- controllers/category.js
+++ controllers/category.js
@@ -1,20 +1,47 @@
-module.exports.getAll = function(req, res) {
-  res.status(200).json({
-    categories: [
-      {
-        id: 1
-      },
-      {
-        id: 2
-      }
-    ]
-  });
+const Category = require('../models/Category');
+const Position = require('../models/Position');
+const errorHandler = require('../utils/errorHandler')
+
+module.exports.getAll = async function (req, res) {
+    try {
+        const categories = await Category.find({user: req.user.id});
+        res.status(200).json(categories);
+    } catch (e) {
+        errorHandler(e, res);
+    }
 };
 
-module.exports.getById = function(req, res) {};
+module.exports.getById = async function (req, res) {
+    try {
+        const category = await Category.findById(req.params.id);
+        res.status(200).json(category);
+    } catch (e) {
+        errorHandler(e, res);
+    }
+};
 
-module.exports.delete = function(req, res) {};
+module.exports.delete = async function (req, res) {
+    try {
+        await Category.remove({_id: req.params.id});
+        await Position.remove({category: req.params.id});
+        res.status(200).json({message: "Removed successfully"})
+    } catch (e) {
+        errorHandler(e, res);
+    }
+};
 
-module.exports.create = function(req, res) {};
+module.exports.create = function (req, res) {
+    try {
 
-module.exports.update = function(req, res) {};
+    } catch (e) {
+        errorHandler(e, res);
+    }
+};
+
+module.exports.update = function (req, res) {
+    try {
+
+    } catch (e) {
+        errorHandler(e, res);
+    }
+};
