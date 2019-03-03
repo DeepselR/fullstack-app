7cf74ef3736567b5c24f77a1c8389e3d2896aaed
Roman Martsinovich <martsinovichroman@gmail.com>
2019-03-03 12:11:38 +0300

    Login/Register Layout

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import "materialaze-css/dist/js/materialize.js";
+import "../node_modules/materialize-css/dist/js/materialize.js";
 
 
 
